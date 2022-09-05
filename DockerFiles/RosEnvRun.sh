ce2f3a497b3c448a0609d4eafd35db72e8e3919d
skrj <skrj@example.com>
2022-09-06 00:56:16 +0900

    [UPDATE] 一部修正

--- DockerFiles/RosEnvRun.sh
+++ DockerFiles/RosEnvRun.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-docker run --rm                                 \
-            -it                                 \
-            --net=host                          \
-            --ipc=host                          \
-            --privileged                        \
-            -e ROS_IP=127.0.0.1                 \
-            -e DISPLAY=$DISPLAY                 \
-            --env="QT_X11_NO_MITSHM=1"          \
-            --device=/dev/dri:/dev/dri          \
-	    -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
-            ros/melodic:cpu
+docker run --rm                                \
+           -it                                 \
+           --net=host                          \
+           --ipc=host                          \
+           --privileged                        \
+           -e ROS_IP=127.0.0.1                 \
+           -e DISPLAY=$DISPLAY                 \
+           --env="QT_X11_NO_MITSHM=1"          \
+           --device=/dev/dri:/dev/dri          \
+           -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
+           ros/melodic:cpu
