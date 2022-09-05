067675234b11b1462af2ef202af972234fb58dc9
skrj <skrj@example.com>
2022-09-06 00:47:30 +0900

    [UPDATE] 一部修正

--- DockerFiles/RosEnvRun.sh
+++ DockerFiles/RosEnvRun.sh
@@ -8,5 +8,5 @@ docker run --rm                                 \
             -e DISPLAY=$DISPLAY                 \
             --env="QT_X11_NO_MITSHM=1"          \
             --device=/dev/dri:/dev/dri          \
-	        -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
+	    -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
             ros/melodic:cpu
