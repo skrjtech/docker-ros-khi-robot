743f1179d953d069c5f68ae09ff85e6d0aea28fe
skrj <skrj@example.com>
2022-09-07 22:37:01 +0900

    [UPDATE] 一部修正

--- DockerFiles/dockerInstall.sh
+++ DockerFiles/dockerInstall.sh
@@ -1,58 +1,80 @@
 #!/bin/bash
 set -e
 
-DOCKER_INSTALL_TARGET_VERSION="20.10.17"
+function dockerRipository() {
+    # 必要なファイルをインストール
+    sudo apt install -y                     \\
+                        curl                \\
+                        gnupg-agent         \\
+                        ca-certificates     \\
+                        apt-transport-https \\
+                        software-properties-common
+    # リポジトリのセット
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+    # sudo apt-key fingerprint 0EBFCD88
+    sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+}
 
-sudo apt update
-sudo apt -y upgrade
-sudo apt-get update
-sudo apt-get -y upgrade
+function dockerNewVersionGet() {
+    sudo apt update
+    sudo apt install docker-ce
+}
 
-# オプションの解析
-# DOCKER_INSTALL: optin n で最新の docker をインストール
-DOCKER_INSTALL=true
-while getopts n OPT
-do
-    case $OPT in 
-        n) DOCKER_INSTALL=false ;;
-        *) ;;
-    esac
-done
+function dockerVersionInstall() {
+    sudo apt update
+    sudo apt install -y docker-engine="$1~xenial"
+}
 
-# 常に存在する場合
-if $DOCKER_INSTALL; then
-    # Docker インストール 確認
-    ## whichでコマンドの存在確認
-    DOCKER_CHECK=false
-    if which docker > /dev/null; then DOCKER_CHECK=true; fi
-    # # Docker バージョン 確認
-    if $DOCKER_CHECK; then
+function DockerGroupIdSet() {
+    sudo usermod -aG docker ${USER}
+    su - ${USER}
+}
+
+function packageCheck() {
+    if printf '%s\n' `ls /usr/share` | grep -qx $1; then
+        echo "true";
+    else
+        echo "false";
+    fi
+}
+
+
+main() {
+    DOCKER_INSTALL_TARGET_VERSION=$1
+    # インストール済みをチェック
+    if `packageCheck docker`; then
+        # バージョンをチェック
         # 結果の代入
         RESULT=$(docker -v)
         # 置換後に配列として代入
         RESULT=(${RESULT//','/''})
         DOCKER_VERSION=${RESULT[2]}
-    fi
-
-    # 現在のバージョンを削除
-    if [ DOCKER_INSTALL_TARGET_VERSION -eq DOCKER_VERSION ]; then 
-        exit 0
+        if [ DOCKER_VERSION = DOCKER_INSTALL_TARGET_VERSION ]; then
+            exit 0;
+        else 
+            sudo apt remove docker docker-engine docker.io docker*
+            
+        fi
     else 
-        sudo apt-get remove docker docker-engine docker.io
-        sudo apt-get install -y docker-engine=$DOCKER_INSTALL_TARGET_VERSION
+        dockerRipository
+        dockerVersionInstall $DOCKER_INSTALL_TARGET_VERSION
+        DockerGroupIdSet
     fi
-    exit 0
-else
-    # 最新のバージョンをダウンロード
-    curl https://get.docker.com | sh && sudo systemctl --now enable docker
-    sudo usermod -aG docker ${USER}
-    exit 0
-fi
+}
 
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
-sudo apt update
-sudo apt install -y docker-engine=$DOCKER_INSTALL_TARGET_VERSION
-sudo usermod -aG docker ${USER}
-su - ${USER}
-id -nG
+# オプションの解析
+# DOCKER_INSTALL: optin -n で最新の docker をインストール
+while getopts n OPT
+do
+    case $OPT in 
+        -n) 
+            # 最新のバージョンでダウンロード
+            dockerRipository
+            dockerNewVersionGet
+            DockerGroupIdSet
+            exit 0
+            ;;
+        *)  main "20.10.17"
+            ;;
+    esac
+done
