d32efdb6f090d8afe48daa3b8cf13740eefe2bae
skrjtech <skrjtech@skrtech.com>
2022-11-20 23:47:04 -0800

    update

---
+++ installer/khi_robot_env.sh
@@ -0,0 +1,18 @@
+
+sudo rosdep init
+rosdep update
+
+mkdir -p ~/ws
+cd ~/ws
+mkdir src
+catkin init
+catkin build
+source ~/ws/devel/setup.bash
+echo "source ~/ws/devel/setup.bash" >> ~/.bashrc
+cd ~/ws/src
+git clone https://github.com/Kawasaki-Robotics/khi_robot.git
+cd ~/ws
+rosdep install -y -r --from-paths src --ignore-src
+source ~/.bashrckhi_robot
+catkin build 
+source ~/.bashrc
